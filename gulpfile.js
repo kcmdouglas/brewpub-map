e77d471bd029676fca835bcb031aefd71d2422e7
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-16 21:18:41 -0800

    Add SASS with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
@@ -5,13 +6,15 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var lib = require('bower-files')({
   "overrides": {
-    "bootstrap": {
+    "bootstrap" : {
       "main": [
         "less/bootstrap.less",
-        "dist/css/bootstrap",
-        "dist/css/bootstrap.js"
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
       ]
     }
   }
@@ -32,6 +35,15 @@ gulp.task("cssBower", function() {
   return gulp.src(lib.ext("css").files)
     .pipe(concat("vendor.css"))
     .pipe(gulp.dest("./build/css"));
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
+    .pipe(browserSync.stream());
 });
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
@@ -64,6 +76,7 @@ gulp.task("build", ["clean"], function() {
     gulp.start("jsBrowserify");
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
 });
 
 gulp.task("serve", function() {
@@ -76,6 +89,7 @@ gulp.task("serve", function() {
 
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch("scss/*.scss", ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify'], function() {
